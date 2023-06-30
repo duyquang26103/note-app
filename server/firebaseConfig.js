5b0c8068eb9a4b6db8869209accc7059595370e3
quang-vuong <quang-vuong@cybozu.vn>
2023-06-30 09:06:17 +0700

    note-app - 1

---
+++ server/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase-admin/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDMwM_rBaDXPVyIpmkVO_Syuug_YqNDVXY",
+    authDomain: "note-app-53530.firebaseapp.com",
+    projectId: "note-app-53530",
+    storageBucket: "note-app-53530.appspot.com",
+    messagingSenderId: "132450049478",
+    appId: "1:132450049478:web:87ae972e8337ea3fb266c7",
+    measurementId: "G-E4VTSLHMVP"
+};
+
+
+initializeApp(firebaseConfig);
